e80d4cd7c05ddd09f21656b335d5c6babda37d14
imranAfzal43A <muhammadimran43a@gmail.com>
2023-01-13 15:59:49 +0500

    initial

--- src/screens/showQoute.js
+++ src/screens/showQoute.js
@@ -1,31 +1,30 @@
+import React from 'react';
+import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
 import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Surface, AppBar, ActivityIndicator } from "@react-native-material/core";
-import { Text, View, Image, TouchableOpacity, FlatList } from 'react-native';
+import { AppBar, ActivityIndicator } from "@react-native-material/core";
+import { Text, View, TouchableOpacity, FlatList } from 'react-native';
 import axios from 'axios';
 import styles from '../styles';
+const addUnitId = __DEV__ ? TestIds.BANNER : 'ca-app-pub-5205427700494339/2255715408';
 export default function ShowQoute() {
-    const [showQoute, setShowqoute] = useState(false);
-    const [author, setAuthor] = useState(null);
-    const [content, setContent] = useState(null);
     const [refresh, setRefresh] = useState(false);
     const [loading, setLoading] = useState(false);
     const [qiDAta, setQIdata] = useState([]);
-    const getImage = async () => {
+    const getQ = async () => {
         setLoading(true)
         try {
             const data = []
-            for (let i = 0; i < 6; i++) {
-                const img = await axios.get('https://random.imagecdn.app/v1/image')
+            for (let i = 0; i < 10; i++) {
                 const qoute = await axios.get('https://api.quotable.io/random')
                 const qiObj = {
                     id: qoute.data._id,
-                    imgU: img.data,
                     author: qoute.data.author,
                     content: qoute.data.content
                 }
                 data[i] = qiObj;
             }
+            data.push({ id: 'show' })
             setLoading(false);
             setQIdata(data);
         } catch (e) {
@@ -33,50 +32,42 @@ export default function ShowQoute() {
         }
     }
     useEffect(() => {
-        getImage();
+        getQ();
     }, [refresh])
     function qiRender({ item }) {
         return (
-            <View style={styles.list}>
-                <TouchableOpacity onPress={() => {
-                    setAuthor(item.author);
-                    setContent(item.content);
-                    setShowqoute(!showQoute);
-                }} >
-                    <Image style={styles.img} source={{ uri: item.imgU }} />
-                    <Text style={styles.imgAuthor}>
-                        {item.author}
+            item.id !== 'show' ?
+                <View style={styles.Qou} >
+                    <Text style={styles.content}>
+                        "{item.content}"
                     </Text>
-                </TouchableOpacity>
-            </View>
+                    <Text style={styles.author}>
+                        --{item.author}
+                    </Text>
+                </View>
+                : <TouchableOpacity style={styles.Qou} onPress={() => setRefresh(!refresh)}><Text style={{ color: '#7B2869', fontWeight: 'bold', fontSize: 18, alignSelf: 'center' }} >Tap to get More</Text></TouchableOpacity>
         )
     }
     return (
         <>
             <AppBar
                 style={{ paddingTop: 15, backgroundColor: '#fff' }}
-                titleStyle={{ color: '#7B2869', fontWeight: 'bold', paddingTop: 10 }}
-                title='Qoute'
-                trailing={() => <TouchableOpacity style={{ padding: 10, marginTop: 10 }} onPress={() => setRefresh(!refresh)}><Image style={{ width: 20, height: 20 }} source={require('../../assets/refresh.png')} /></TouchableOpacity>}
+                titleStyle={{ color: '#7B2869', fontWeight: 'bold', paddingTop: 10, alignSelf: 'center' }}
+                title='Quote'
             />
             <View style={styles.container}>
-                <Text style={styles.title} >Qoute Motivation</Text>
+                <Text style={styles.title} >Motivational Quotes</Text>
+                <View style={{ flexDirection: 'row' }}>
+                </View>
                 {!loading ?
                     <FlatList horizontal={true} data={qiDAta} renderItem={qiRender} keyExtractor={(item) => item.id} />
-                    : <ActivityIndicator size={'large'} color='white' />}
-                {showQoute ?
-                    <Surface elevation={5} style={styles.surface}>
-                        <Text style={styles.content}>
-                            {content}
-                        </Text>
-                        <Text style={styles.author}>
-                            {author}
-                        </Text>
-                    </Surface>
-                    : null
-                }
+                    : <ActivityIndicator style={{ flex: 1, justifyContent: 'center' }} size={'large'} color='white' />}
             </View>
             <StatusBar style='auto' />
+            <BannerAd
+                unitId={addUnitId}
+                size={BannerAdSize.FULL_BANNER}
+            />
         </>
     )
 }
--- src/styles.js
+++ src/styles.js
@@ -1,22 +1,13 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
+const sc = Dimensions.get('window');
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#7B2869',
-        alignItems: 'center',
     },
-    title: { fontSize: 22, fontWeight:'bold',padding: 10,color:'white' },
-    list: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        margin: 5,
-        width: 200, height: 300, borderRadius: 15,
-    },
-    imgAuthor: { fontSize: 14, fontWeight: 'bold',padding:4, bottom: 10, position: 'absolute', color: 'white' },
-    img: { width: 200, height: 300, borderRadius: 15,borderColor:'white',borderWidth:1, },
-    surface: { width: '80%', height: 230, borderRadius: 10, backgroundColor: 'white', margin: 10 },
-    content: { fontSize: 14, fontWeight: 'bold', alignSelf: 'center', padding: 20 ,color:'#7B2869'},
-    author: { fontSize: 18, fontWeight: 'bold', bottom: 10, position: 'absolute', alignSelf: 'center',color:'#7B2869' }
+    title: { fontSize: 22, fontWeight: 'bold', padding: 10, color: 'white', alignSelf: 'center' },
+    Qou: { alignSelf: 'center', alignContent: 'center', justifyContent: 'center', backgroundColor: 'white', width: sc.width / 1.1, height: sc.width / 1.4, margin: 4, borderRadius: 15 },
+    content: { fontSize: 20, fontWeight: 'bold', alignSelf: 'center', padding: 20, color: '#7B2869' },
+    author: { fontSize: 18, fontWeight: 'bold', bottom: 10, position: 'absolute', alignSelf: 'center', color: '#7B2869' }
 })
 export default styles;
